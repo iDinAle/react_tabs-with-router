9c2035868cc2091433f0615300dac7a80234ead7
Alexii Aidinov <alexii.aidinov@gmail.com>
2019-12-25 20:14:36 +0200

    Updates

---
+++ precache-manifest.0275de2a875e68d81d60a0fb7496a454.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60f0a2a7a7d25f0ef4e05ee2e57af5f6",
+    "url": "/react_tabs-with-router/index.html"
+  },
+  {
+    "revision": "8cc607461bf05ae352ab",
+    "url": "/react_tabs-with-router/static/css/main.892bc39a.chunk.css"
+  },
+  {
+    "revision": "c6d38b044a4284916b35",
+    "url": "/react_tabs-with-router/static/js/2.0ba52e99.chunk.js"
+  },
+  {
+    "revision": "8cc607461bf05ae352ab",
+    "url": "/react_tabs-with-router/static/js/main.a1cd39e6.chunk.js"
+  },
+  {
+    "revision": "da3ca6fceb301548d22d",
+    "url": "/react_tabs-with-router/static/js/runtime~main.5858125e.js"
+  }
+]);
